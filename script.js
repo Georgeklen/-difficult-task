0c914d38f0ce78da476fbebc2e36850b26f54170
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-17 22:18:07 +0700

    fix:(Исправил недочет)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num = 266219;
+const num = 266219;
 let transNum = (String(266219));
 alert (transNum);
 let container = ((transNum[0] * transNum[1] * transNum[2] * transNum[3] * transNum[4] * transNum[5])**3);
