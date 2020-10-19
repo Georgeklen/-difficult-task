91246b0d2e8c31e12c24d13eb14fd6df5f5c11ee
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-10-19 09:54:38 +0700

    fix: bag number

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-
-
-
-
-    prompt ("?")
-  function test(str) {
-    if (typeof str !== 'string') {
-        return ('аргумент не строка');
-      
-    }
-
-   return str.length > 30 ? str.slice(0, 30) + '... \n' : str ;
+let asd =      123456789123456789012345678901234567890909090;
+function test(str) {
+  if (typeof str!== 'string') {
+      return ('аргумент не строка');
+    
+  } else {
+    console.log('аргумент строка')
   }
-  console.log(test())
- 
+
+ return str.length > 30 ? str.slice(0, 30) + '... \n' : str ;
+}
+console.log(test(asd).trim())
